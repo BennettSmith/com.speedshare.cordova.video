1b3b5e280eb1edab21a787da1a03e98aaa5cb4b5
Edward Parsons <edparsons2@gmail.com>
2015-05-11 15:03:32 -0700

    debugging videokit script

--- scripts/downloadiOSVideoKit.js
+++ scripts/downloadiOSVideoKit.js
@@ -6,17 +6,20 @@ module.exports = function (context) {
     Q = context.requireCordovaModule('q'),
     deferral = new Q.defer();
   console.log('Downloading iOS VideoKit');
-  downloadFile('https://s3.amazonaws.com/artifact.tokbox.com/rel/ios-sdk/VideoKit_2.3_lc.zip', './VideoKit_2.3_lc.zip', function (err) {
+  downloadFile('https://s3-us-west-2.amazonaws.com/speedshare/VideoKit_2.3_lc.zip', './VideoKit_2.3_lc.zip', function (err) {
     if (!err) {
       console.log('downloaded');
       exec('unzip -P \'BUAQ!R8RHabK7XrGfUKH\' ./VideoKit_2.3_lc.zip', function (err, out, code) {
         console.log('expanded');
         var frameworkDir = context.opts.plugin.dir + '/src/ios/VideoKit';
+        var frameworkDir = './plugins/com.speedshare.cordova.video/src/ios/VideoKit';
         exec('mv ./VideoKit/VideoKit ' + frameworkDir, function (err, out, code) {
           console.log('moved VideoKit ' + frameworkDir);
           console.log('cleaning up');
           exec('rm ./VideoKit', function (err, out, code) {
-            deferral.resolve();
+            exec('rm ./VideoKit_2.3_lc.zip', function (err, out, code) {
+              deferral.resolve();
+            });
           });
         });
       });
