44a0f586e56ea23247c5f3fe2764774b2e318795
Edward Parsons <edparsons2@gmail.com>
2015-05-11 13:53:43 -0700

    Adding the downloading of video kit to the plugin code

---
+++ scripts/downloadiOSVideoKit.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+module.exports = function (context) {
+  var downloadFile = require('./downloadFile.js'),
+    exec = require('./exec/exec.js'),
+    Q = context.requireCordovaModule('q'),
+    deferral = new Q.defer();
+  console.log('Downloading iOS VideoKit');
+  downloadFile('https://s3.amazonaws.com/artifact.tokbox.com/rel/ios-sdk/VideoKit_2.3_lc.zip', './VideoKit_2.3_lc.zip', function (err) {
+    if (!err) {
+      console.log('downloaded');
+      exec('unzip -P \'BUAQ!R8RHabK7XrGfUKH\' ./VideoKit_2.3_lc.zip', function (err, out, code) {
+        console.log('expanded');
+        var frameworkDir = context.opts.plugin.dir + '/src/ios/VideoKit';
+        exec('mv ./VideoKit/VideoKit ' + frameworkDir, function (err, out, code) {
+          console.log('moved VideoKit ' + frameworkDir);
+          console.log('cleaning up');
+          exec('rm ./VideoKit', function (err, out, code) {
+            deferral.resolve();
+          });
+        });
+      });
+    }
+  });
+  return deferral.promise;
+};
