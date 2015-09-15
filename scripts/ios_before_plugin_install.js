cba9279b37e6862f8cb87c5589064c64307ac60a
Bennett Smith <bennett@focalshift.com>
2015-09-15 09:30:53 -0700

    Replaces iOS plugin install script
    Replaces the old download script with a new one that does not rely
    directly on the exec package. Instead it is using a pure nodejs zip
    extraction package.

---
+++ scripts/ios_before_plugin_install.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+module.exports = function (context) {
+  if (ctx.opts.platforms.indexOf('ios') < 0) {
+    return;
+  }
+
+  var downloadFile = require('./downloadFile.js'),
+    extract = require('extract-zip'),
+    videoKitURL = 'https://s3-us-west-2.amazonaws.com/speedshare/VideoKit.zip',
+    destDir = context.opts.plugin.dir + '/src/ios/';
+
+  console.log('Downloading iOS VideoKit');
+  console.log('  URL = ' + videoKitURL);
+
+  downloadFile(videoKitURL, destDir + 'VideoKit.zip', function (err) {
+    if (!err) {
+      console.log('Downloaded!');
+      extract(destDir+'VideoKit.zip', {dir: destDir}, function(err) {
+      });
+    }
+  });
+
+  return deferral.promise;
+};
