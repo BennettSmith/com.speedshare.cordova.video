d247f634989f402cdf0410a1d68d0e3bf76e7b5d
Bennett Smith <bennett@focalshift.com>
2015-09-15 17:27:49 -0700

    Syncronous download of VideoKit

--- scripts/ios_before_plugin_install.js
+++ scripts/ios_before_plugin_install.js
@@ -1,24 +1,21 @@
 #!/usr/bin/env node
 
 module.exports = function (context) {
-  var downloadFile = require('./downloadFile.js'),
-    fs = require('fs'),
-    extract = require('extract-zip'),
-    videoKitURL = 'https://s3-us-west-2.amazonaws.com/speedshare/VideoKit.zip',
-    destDir = context.opts.plugin.dir + '/src/ios/';
+  var urllib = require('urllib-sync'),
+      fs = require('fs'),
+      videoKitURL = 'https://s3-us-west-2.amazonaws.com/speedshare/VideoKit.zip',
+      destDir = context.opts.plugin.dir + '/src/ios/';
 
   console.log('Downloading iOS VideoKit');
   console.log('  URL = ' + videoKitURL);
 
-  downloadFile(videoKitURL, destDir + 'VideoKit.zip', function (err) {
-    if (!err) {
-      console.log('Downloaded!');
-      extract(destDir+'VideoKit.zip', {dir: destDir}, function(err) {
-        if (!err) {
-          fs.unlink(destDir+'VideoKit.zip', function(err) {
-          });
-        }
-      });
-    }
-  });
+  var response = urllib.request(videoKitURL, { writeFile: destDir + 'VideoKit.zip' });
+  if (response.status = 200) {
+    console.log('Downloaded!');
+  }
+  else {
+    console.log('Failed to download. Status = ' + response.status);
+  }
+  // fs.unlink(destDir+'VideoKit.zip', function(err) {
+  // });
 };
